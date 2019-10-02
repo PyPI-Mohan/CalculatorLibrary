b241090bbbee43013ec15abfcdec243f158b9e53
mohanjogi <53114518+mohanjogi@users.noreply.github.com>
2019-10-02 21:33:05 +0530

    new file added

---
+++ calculator.py
@@ -0,0 +1,10 @@
+"""
+Calculator library containing basic math operations
+
+"""
+
+def add( first_term, second_term):
+    return first_term + second_term
+
+def sutract( first_term, second_term):
+    return first_term - second_term
